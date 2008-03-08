f011ebe9898ec3c6800d8e90312361f866f9ad60
calin.culianu <calin.culianu@a51dfd64-7346-0410-b801-27bcb2126d9c>
2008-03-08 03:02:24 +0000

    - Made error messaging more informative in FSM Client 'ReceiveOK.m'

--- Client/Matlab/@RTLSM/ReceiveOK.m
+++ Client/Matlab/@RTLSM/ReceiveOK.m
@@ -1,6 +1,13 @@
 function [] = ReceiveOK(sm, cmd)
   lines = FSMClient('readlines', sm.handle);
   [m,n] = size(lines);
-  line = lines(1,1:n);
-  if isempty(findstr('OK', line)),  error(sprintf('RTLinux FSM Server did not send OK after %s command.', cmd)); end;
+  line = '';
+  if (m & n),
+      line = lines(1,1:n);
+  end;
+  if isempty(findstr('OK', line)),  
+      errstr = sprintf('RTLinux FSM Server did not send OK after %s command.', cmd);
+      if (m | n), errstr = sprintf('%s\nInstead it sent:\n\n >> %s', errstr, line); end;      
+      error(sprintf('%s\n', errstr)); 
+  end;
 end
