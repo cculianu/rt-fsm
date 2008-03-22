eba9162c54c7e4c68c9af5719d89e0e5580196b4
calin.culianu <calin.culianu@a51dfd64-7346-0410-b801-27bcb2126d9c>
2008-03-22 16:00:26 +0000

    - Fixed bug in matlab SetOutputRouting where you couldn't do DOUT of only 1 channel - Fixed FSM had a bug whereby scheduled wave channels weren't being unlocked properly in certain cases if scheduled waves were cleared while they were running

--- trunk/include/Version.h
+++ trunk/include/Version.h
@@ -1,7 +1,7 @@
 #ifndef FSM_VERSION_H
 #define FSM_VERSION_H
 
-#define VersionSTR "2008.03.19 EmbC"
-#define VersionNUM 220080319UL
+#define VersionSTR "2008.03.22 EmbC"
+#define VersionNUM 220080322UL
 
 #endif
