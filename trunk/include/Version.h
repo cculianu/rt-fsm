6c3afc0155fd1712b1c53400c53e3dbf0e4ae319
calin.culianu <calin.culianu@a51dfd64-7346-0410-b801-27bcb2126d9c>
2008-06-11 14:13:05 +0000

    Oops -- forgot to bump version.  Bumped version number to 2008.06.11 EmbC

--- trunk/include/Version.h
+++ trunk/include/Version.h
@@ -1,7 +1,7 @@
 #ifndef FSM_VERSION_H
 #define FSM_VERSION_H
 
-#define VersionSTR "2008.05.23 EmbC"
-#define VersionNUM 220080523UL
+#define VersionSTR "2008.06.11 EmbC"
+#define VersionNUM 220080611UL
 
 #endif
