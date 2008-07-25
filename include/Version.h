bb50a00960192ba0ae13c3e5492e171e9229e71f
calin.culianu <calin.culianu@a51dfd64-7346-0410-b801-27bcb2126d9c>
2008-07-25 17:24:49 +0000

    - Bumped version - Optimized generation of Embedded C code to generate a 2D matrix   for matrix cells that are purely numeric, and otherwise   generate a big function for matrix cells that are non-numeric.
      This hopefully makes compilation much easier/faster.

--- include/Version.h
+++ include/Version.h
@@ -1,7 +1,7 @@
 #ifndef FSM_VERSION_H
 #define FSM_VERSION_H
 
-#define VersionSTR "2008.07.24 EmbC"
-#define VersionNUM 220080724UL
+#define VersionSTR "2008.07.25 EmbC"
+#define VersionNUM 220080725UL
 
 #endif
