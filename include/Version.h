521424c8f13f866547cd4908673519eca91dac2f
calin.culianu <calin.culianu@a51dfd64-7346-0410-b801-27bcb2126d9c>
2008-08-13 17:52:04 +0000

    - Bumped version - Fixed bug in SAMPLE_TO_VOLTS() (which was also affecting custom threshold detection in embedded C) - Updated ChangeLog

--- include/Version.h
+++ include/Version.h
@@ -1,7 +1,7 @@
 #ifndef FSM_VERSION_H
 #define FSM_VERSION_H
 
-#define VersionSTR "2008.08.08 EmbC"
-#define VersionNUM 220080808UL
+#define VersionSTR "2008.08.13 EmbC"
+#define VersionNUM 220080813UL
 
 #endif
