47ba11e39363c2c204c67922ddbe7842bd4fb101
calin.culianu <calin.culianu@a51dfd64-7346-0410-b801-27bcb2126d9c>
2008-09-25 07:35:23 +0000

    - Modified the SoundServer to use libsamplerate to do resampling of   incoming sound files.  This should fix the problems/aliases/artifacts   introduced into resampled sounds (previous method used linear   interpolation without a band-pass filter which is bad, bad, bad).  Now   sounds should play ok even if the incoming sounds aren't at 44.1kHz (the   native rate of the emulator). Note, for performance it is still   recommended that you generate and upload sounds at 44.1kHz.

--- addons/SoundTrig/WavFile.cpp
+++ addons/SoundTrig/WavFile.cpp
@@ -3,6 +3,7 @@
 #include <fstream>
 #include "IntTypes.h"
 #include <string.h>
+#include "libresample/libresample.h"
 
 namespace 
 {
@@ -159,26 +160,55 @@ namespace
 // NB: when writing data, channels should be interleaved as in sample0{chan0,chan1},sample1{chan0,chan1} etc..
 bool OWavFile::write(const double *data, unsigned size, unsigned srate, double scale_min, double scale_max)
 {
-  double factor = 1.0;
+    unsigned nwrit = 0, nframes = size/p->nchans; // nb: nframes gets changed below if we resample
+    double *data_ptr = 0; // if non-null, points to resampled interleaved data -- gets deleted
 
-  // NB: need to upsample/downsample here.. ergh we use a scale factor that turns out to be how much we increment our frame loop index by below..
+  // NB: need to upsample/downsample here.. use libresample and resample to another memory buffer
   if (srate != p->srate) {
-    factor = double(srate)/double(p->srate);    
+    int min_frames = -1;
+    double factor = double(p->srate)/double(srate);
+    int n_in = nframes, space_out = int(Ceil(nframes*factor + factor) + 1.);
+    double *ddata = data_ptr = new double[space_out * p->nchans];
+    memset(ddata, 0, sizeof(double)*(space_out * p->nchans));
+    float *data_in = new float[n_in];
+    float *data_out = new float[space_out];
+
+    for (unsigned ch = 0; ch < p->nchans; ++ch) {
+        for (unsigned frame = 0; frame < nframes; ++frame) {
+            // deinterleave data and convert to float
+            data_in[frame] = data[frame*p->nchans + ch];
+        }
+        int in_used = 0;
+        void *h = resample_open(1, 1/25., 25.);
+        int ret = resample_process(h, factor, data_in, n_in, 1, &in_used, data_out, space_out);
+        resample_close(h);
+        if (ret > -1 && ( ret < min_frames || min_frames < 0 ) ) 
+            min_frames = ret;
+        double mind = 2e9, maxd = -2e9;  
+        for (int frame = 0; frame < ret; ++frame) {
+            double d = data_out[frame];
+            ddata[frame*p->nchans + ch] = d;
+            // figure out data max and min for normalization phase below..
+            if (mind > d) mind = d;
+            if (maxd < d) maxd = d;
+        }
+        // now normalize data
+        for (int frame = 0; frame < ret; ++frame) {
+            double & d = ddata[frame*p->nchans + ch];
+            d = ((d-mind)/(maxd-mind)) * (scale_max-scale_min) + scale_min;
+        }
+    }
+    delete [] data_in;
+    delete [] data_out;
+    data = ddata;
+    nframes = min_frames > -1 ? min_frames : 0;
   }
 
-  unsigned nwrit = 0, nframes = size/p->nchans;
-  double fi, ci;
-  for (double i = 0; (fi = Floor(i)) < nframes; i += factor, ++nwrit) {
-    ci = Ceil(i);      
+  for (unsigned frame = 0; frame < nframes; ++frame, ++nwrit) {
     for (unsigned chan = 0; chan < p->nchans; ++chan) {
-      unsigned idx = unsigned(fi)*p->nchans+chan, idx2 = unsigned(ci)*p->nchans+chan;
-      if (ci >= nframes) idx2 = idx;
+      unsigned idx = frame*p->nchans+chan;
       double datum = data[idx];
-      if (idx != idx2) {
-          double datum2 = data[idx2];
-          datum = datum*(1.-(i-fi)) + datum2*(1.-(ci-i));          
-      }
-      uint32 samp = uint32((datum-scale_min)/(scale_max-scale_min) * double(~0U)); // 32-bit unsigned sample
+      uint32 samp = uint32((datum-scale_min)/(scale_max-scale_min) * double(0xfffffffaU) + 1); // 32-bit unsigned sample -- note we never use full 32-bit range because we were getting clipping errors sometimes probably due to floating point inaccuracies?
       if (p->bitspersample != 8) {
         // argh, deal with signed PCM
         samp -= 0x7fffffff;
@@ -190,6 +220,7 @@ bool OWavFile::write(const double *data, unsigned size, unsigned srate, double s
     if (!p->f.good()) break;
   }
   p->nframes += nwrit;
+  if (data_ptr) delete [] data_ptr;
   return isOk();
 }
 
@@ -199,7 +230,7 @@ bool OWavFile::write(const void *v,
                      unsigned srate)
 {
   const uint8 *d = static_cast<const uint8 *>(v);  
-  double factor = 1.0, scale_min, scale_range;
+  double scale_min, scale_range;
 
   switch (bits) {
     case 8: scale_min = 0., scale_range = 256.; break;
@@ -208,50 +239,27 @@ bool OWavFile::write(const void *v,
     case 32: scale_min = -2147483648., scale_range = 4294967296.; break;
     default: return false;
   }
-  
-  // NB: need to upsample/downsample here.. ergh we use a scale factor that turns out to be how much we increment our frame loop index by below..
-  if (srate != p->srate) {
-    factor = double(srate)/double(p->srate);    
-  }
-  
-  unsigned nwrit = 0, nframes = size/p->nchans;
-  double fi, ci;
-  for (double i = 0; (fi = Floor(i)) < nframes; i += factor, ++nwrit) {
-    ci = Ceil(i);
-    for (unsigned chan = 0; chan < p->nchans; ++chan) {
-      unsigned idx = unsigned(fi)*p->nchans+chan, idx2 = unsigned(ci)*p->nchans+chan;
-      if (ci >= nframes) idx2 = idx;
-      uint32 samp;
-      double datum = 0., datum2 = 0.;
+  // convert to doubles in memory, then pass to the other write function
+  double *dbls = new double[size];
+  for (unsigned i = 0; i < size; ++i) {
+      double & datum = dbls[i];
       switch (bits) {
-        case 8:  datum = d[idx]; datum2 = d[idx2]; break;
-        case 16: datum = ((int16 *)d)[idx]; datum2 = ((int16 *)d)[idx2]; break;
-        case 24: 
-          samp = 0;
-          memcpy(&samp, d+idx*3, 3);
+      case 8:  datum = d[i]; break;
+      case 16: datum = ((int16 *)d)[i]; break;
+      case 24: {
+          uint32 samp = 0;
+          memcpy(&samp, d+i*3, 3);
           if (isBigEndian()) samp >>= 8;
           datum = samp;
-          memcpy(&samp, d+idx2*3, 3);
-          if (isBigEndian()) samp >>= 8;
-          datum2 = samp;
+      }
           break;
-        case 32: datum = ((int32 *)d)[idx]; datum2 = ((int32 *)d)[idx2]; break;
+      case 32: datum = ((int32 *)d)[i];  break;
       }
-      if (idx != idx2) // interpolate
-          datum = datum*(1.-(i-fi)) + datum2*(1.-(ci-i));
-      samp = uint32((datum-scale_min)/(scale_range) * double(~0U));
-      if (p->bitspersample != 8) {
-        // argh, deal with signed PCM
-        samp -= 0x7fffffff;
-      }
-      samp = samp >> (32-p->bitspersample); // take the upper BitsPerSample bits
-      samp = LE(samp); // make sure it's little endian so that the write command below 1. succeeds on all platforms (we write the first bitspersaple bits) and 2. wave files are anyway little endian
-      p->f.write((const char *)&samp, p->bitspersample/8);
-    }
-    if (!p->f.good()) break;
+      datum = (datum-scale_min)/scale_range;
   }
-  p->nframes += nwrit;
-  return isOk();
+  bool ret = write(dbls, size, srate, 0., 1.0);
+  delete [] dbls;
+  return ret;
 }
 
 namespace 
