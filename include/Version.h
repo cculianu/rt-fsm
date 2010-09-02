5c9baadc1474d0bb5b8e859646d064e367c7c581
carlosbrody <carlosbrody@a51dfd64-7346-0410-b801-27bcb2126d9c>
2010-09-02 21:16:56 +0000

    Reported version is now as of 2-Sep-2010

--- include/Version.h
+++ include/Version.h
@@ -1,8 +1,8 @@
 #ifndef FSM_VERSION_H
 #define FSM_VERSION_H
 
-#define VersionSTR_Base "2009.07.05"
-#define VersionNUM      220090705UL
+#define VersionSTR_Base "2010.09.02"
+#define VersionNUM      220100902UL
 
 #ifdef EMULATOR
 #  define VersionSTR  VersionSTR_Base " EmbC.Emul"
